c23f2f251de9fb49984191329e19e7e09da78a9e
akosENagy <akos.e.nagy@gmail.com>
2017-05-13 15:31:30 +0200

    Initialized psycopg2 cursor in main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+
+def main():
+    try:
+        # setup connection string
+        connect_str = "dbname='aakeeka' user='aakeeka' host='localhost' password='postgresql'"
+        # use our connection values to establish a connection
+        conn = psycopg2.connect(connect_str)
+        # set autocommit option, to do every query when we call it
+        conn.autocommit = True
+        # create a psycopg2 cursor that can execute queries
+        cursor = conn.cursor()
+
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
+
+if __name__ == '__main__':
+    main()
