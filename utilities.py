6b32cba818a91e5104ae024977bd2f539f7058c2
akosENagy <akos.e.nagy@gmail.com>
2017-05-30 18:30:13 +0200

    Added header appending to run_select_query() in utilities.py

--- utilities.py
+++ utilities.py
@@ -13,14 +13,16 @@ def init_db_connection(connection_string=CONNECTION_STRING):
     conn.autocommit = True
     # create a psycopg2 cursor that can execute queries
     cursor = conn.cursor()
-
     return cursor
 
 
-def run_select_query(query_string):
+def run_select_query(query_string, header):
     '''
-    Receives a query string for a select statement, returns results as a list of tuples.
+    Receives a query string for a select statement, appends a header and returns results as a list of tuples.
     '''
     cursor = init_db_connection()
     cursor.execute(query_string)
-    return cursor.fetchall()
+    results = cursor.fetchall()
+    results.insert(0, header)
+
+    return results
