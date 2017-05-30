563d7c8ab38713df9d93c626bee8b25d9e2d076a
akosENagy <akos.e.nagy@gmail.com>
2017-05-30 17:15:13 +0200

    Added main page template (index.html) base and its render function to main.py

--- main.py
+++ main.py
@@ -7,9 +7,17 @@
 import psycopg2
 import display
 import utilities
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='/static')
+
+
+# ROUTE FUNCTIONS
+####################################################################
+
+@app.route('/')
+def render_main_page():
+    return render_template('index.html')
 
 
 # MAIN
